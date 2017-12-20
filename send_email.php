61e045a07114e68aa18970a243695d06e29b33fb
Maarten Scholz <maarten.scholz@gmail.com>
2017-12-21 00:23:59 +0100

    Send mail with swiftmailer

--- send_email.php
+++ send_email.php
@@ -1,3 +1,18 @@
 <?php
 
-mail('maarten.scholz@gmail.com', 'Test', 'test');
+require_once 'vendor/autoload.php';
+
+$transport = (new Swift_SmtpTransport('smtp.google.com', 465))
+    ->setUsername(getenv('SMTP_USERNAME'))
+    ->setPassword(getenv('SMTP_PASSWORD'));
+
+$mailer = new Swift_Mailer($transport);
+
+$mail = (new Swift_Message('Formulier verzonden'))
+    ->setFrom(['info@voetbalmateriaal.com' => 'Expo Line'])
+    ->setTo('maarten.scholz@gmail.com')
+    ->setBody(implode('; ', $_POST));
+
+$mailer->send($mail);
+
+header('location: /');
