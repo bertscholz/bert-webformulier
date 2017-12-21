9d84027e5e002a3a3872365b7aae67050cf50054
Maarten Scholz <maarten.scholz@gmail.com>
2017-12-21 01:37:34 +0100

    Email markup

--- send_email.php
+++ send_email.php
@@ -9,7 +9,7 @@ $mailer = new Swift_Mailer($transport);
 $mail = (new Swift_Message('Formulier verzonden'))
     ->setFrom(['info@voetbalmateriaal.com' => 'Expo Line'])
     ->setTo('maarten.scholz@gmail.com')
-    ->setBody(implode('<br>', $_POST));
+    ->setBody(implode('; ', $_POST));
 
 $mailer->send($mail);
 
